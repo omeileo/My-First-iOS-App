b043bc3cc22b56c0d321a0e39098c4166af08bd1
Jase-Omeileo West <omeileo@icloud.com>
2017-06-10 00:12:03 -0500

    First Commit

--- My First iOS App/ViewController.swift
+++ My First iOS App/ViewController.swift
@@ -8,18 +8,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+class ViewController: UIViewController
+{
+    @IBOutlet weak var label_name: UILabel!
+    var tap_count = 0
+    
+    @IBAction func button_changeName(_ sender: Any)
+    {
+        label_name.text = "Jase-Omeileo West"
+        tap_count += 1
+        
+        if tap_count >= 10
+        {
+            label_name.text = "Congrats! You've unlucked the annoying award. Woo."
+        }
+    }
+    
+    @IBAction func button_changeName2(_ sender: Any)
+    {
+        label_name.text = "Buttons are cool!"
+    }
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
-    override func didReceiveMemoryWarning() {
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
